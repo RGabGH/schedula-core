c37557bba505ab578bea7ffd4c51eacc98341569
Raffaele Gabrielli <raffaele.gabrielli@overcore.it>
2026-02-24 16:41:48 +0100

    pesante

--- data/_writer.js
+++
@@ -1,9 +0,0 @@
-var fs=require("fs");
-var lines=[];
-lines.push('var hotelData = {');
-lines.push('    Resources: [');
-lines.push('        {');
-lines.push('            Id: "DLX-101", Name: "101 - Deluxe Vista Mare",');
-lines.push('            Group: 1,');
-lines.push('            Data: { Beds: "1 King", Amenities: ["Vista Mare", "Jacuzzi"], Rate: 450, Floor: 1 },');
-lines.push('            Items: [');
